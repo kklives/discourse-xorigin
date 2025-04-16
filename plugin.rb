0ad3b2f327f0edb8570d656ba5f3b1ebf70a09fb
kklives <yanyants@gmail.com>
2025-04-16 08:57:29 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,6 @@
 # authors: Julian
 
 Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
-Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => '*'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => '172.100.25.12'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS, DELETE'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'})
